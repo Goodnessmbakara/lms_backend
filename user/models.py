c3979d5f8b2b8fc088b13f45f695c47cf5a9fcc2
Goodnessmbakara <amicablembakara50@gmail.com>
2024-06-27 21:26:33 +0100

    set up a user model for authentication

---
+++ user/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    username = models.CharField(max_length=100, unique = True)
+    email = models.EmailField(unique=True)
+    is_librarian = models.BooleanField(default=False)
+    
+    
